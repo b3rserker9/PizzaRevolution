f5e3de6cd4062ee0c1934f3c07e08f72bdab4a03
b3rserker9 <aliyoussef3c@gmail.com>
2021-10-22 16:32:52 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/pizza/PizzaRevolutionApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaRevolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaRevolutionApplication.class, args);
+	}
+
+}
